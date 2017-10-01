565e36e7dc326e6b566291cdd46d5aa21de6098e
sirpy <hrot@knil.co>
2017-10-01 16:22:31 +0300

    add: use minified version of sidebox

---
+++ js/apploader.js
@@ -0,0 +1,19 @@
+
+window.applozic = window.applozic || {};
+var MCK_CONTEXTPATH = "/packages/knil_applozic-web";
+var MCK_STATICPATH = "/packages/knil_applozic-web";
+var MCK_ONINIT = "";
+// $.getScript(MCK_STATICPATH + '/sidebox/js/app/mck-app.js');
+var options = applozic._globals;
+if (typeof options !== 'undefined') {
+    MCK_ONINIT = options.onInit;
+}
+window.addEventListener('error', function (e) {
+    if (typeof (e.target.src) !== 'undefined' &&  e.target.src.indexOf('sidebox') !== -1 && typeof MCK_ONINIT === 'function') {
+        console.log("Plugin loading error. Refresh page.");
+        MCK_ONINIT("error");
+    }
+}, true);
+var imported = document.createElement('script');
+imported.src = MCK_STATICPATH + '/js/app/sidebox/app.js';
+document.head.appendChild(imported);
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'knil:applozic-web',
-  version: '0.1.6',
+  version: '0.1.7',
   // Brief, one-line summary of the package.
   summary: 'Web client and javascript api for applozic chat',
   // URL to the Git repository containing the source code for this package.
